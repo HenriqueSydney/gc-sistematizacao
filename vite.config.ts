8118b00bf00c0afa11be75d99fa6f1c3dd45f1b4
Henrique Sydney Ribeiro Lima <henrique.lima@rfb.gov.br>
2025-03-09 16:27:32 -0300

    Test covarage

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,10 @@ export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+    coverage: {
+      provider: 'v8', // ou 'c8', dependendo da sua escolha
+      reporter: ['text', 'html'], // Gera relatório no terminal e no HTML
+      reportsDirectory: './coverage',
+    },
   },
 })
